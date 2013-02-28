c77dcdeeba5ddc13cd80544878296d5aec4f7cac
Molly White <molly.white5@gmail.com>
2013-02-28 14:25:32 -0500

    More tokens

--- tokenizer.py
+++ tokenizer.py
@@ -21,7 +21,7 @@
 
 __all__ = ['Tokenizer']
 
-import codecs, logging, os
+import codecs, logging, os, re
 import lex
 
 class Tokenizer(object):
@@ -30,31 +30,97 @@ class Tokenizer(object):
               'NOINCLUDE', # <noinclude>
               'E_NOINCLUDE', # </noinclude>
               'PAGEQUALITY', # <pagequality level="4" user="GorillaWarfare" />
+              'EXTRANEOUS_HTML', # <div>, <p>, etc.
+              'DECLASSIFIED', # Declassified per Executive Order... Date: 2011
+              'SECRET', # TOP SECRET - Sensitive
+              'TABLE', # Beginning of table <table>
+              'E_TABLE', # End of table </table>
+              'TROW', # Beginning of table row <tr>
+              'E_TROW', # End of table row </tr>
+              'TITEM', # Beginning of table item <td>
+              'E_TITEM', # End of table item, indicated by a newline in table state
+              'OLIST', # Ordered list <ol>
+              'E_OLIST', # End of ordered list </ol>
+              'LITEM', # List item <li>
+              'E_LITEM', # End of list item </li>
               'WLINK', # For links to Wikipedia, Wiktionary, etc.
+              'UNDERLINED', # {{u|...}}
+              'ITALICIZED', # ''...''
+              'BOLDED', # '''...'''
+              'WHITESPACE',
               'WORD',
-              'SPACE',
+              'PUNCT',
+              'NUMBER',
+              'LTGT'
+              )
+    
+    states = (
+              ('table', 'inclusive'),
               )
 
     # Simple matches
-    t_NOINCLUDE = r'<noinclude>'
-    t_E_NOINCLUDE = r'</noinclude>'
+    t_NOINCLUDE = r'<noinclude>\n?'
+    t_E_NOINCLUDE = r'</noinclude>\n?'
+    t_table_TROW = r'<tr>\n?'
+    t_table_E_TROW = r'</tr>\n?'
+    t_table_TITEM = r'<td(.*?)>'
+    t_table_E_TITEM = r'\n'
+    t_OLIST = r'<ol>'
+    t_E_OLIST = r'</ol>'
+    t_LITEM = r'<li>'
+    t_E_LITEM = r'</li>'
+    t_DECLASSIFIED = r'<b>Declassified(?:.*?)Date\:\s2011'
+    t_SECRET = r'\{{2}center\|<u>TOP SECRET – Sensitive</u>\}{2}'
     t_WLINK = r'[[]{2}w(?:ikt)?:(?:.*?)[]]{2}'
+    t_PUNCT = r"""[!@\#\$\%\^&\*\(\)\-–—\+=\[\]\{\}\\\|\:;"',\.\?/~]"""
     t_WORD = r'[a-zA-Z]+'
-    t_SPACE = r'[\s\t\r\n]'
+    t_NUMBER = r'[0-9]+'
+    t_WHITESPACE = r'(?:\s|\t|\r|\n|<br\s?/?>)'
+    t_LTGT = r'[<>]'
         
-    def __init__(self):
-        self.logger = logging.getLogger("W2L")
-
-    # More complex matches
+    # More complex match behavior  
+    def t_TABLE(self, token):
+        r'<table(?:.*?)>\n'
+        token.lexer.begin('table') # Begin table state
+        return token
+        
+    def t_E_TABLE(self, token):
+        r'</table>\n'
+        token.lexer.begin('INITIAL') # End table state
+        return token
+    
     def t_PAGEQUALITY(self, token):
         r'<pagequality\slevel="(\d)"\suser="(?:\S*?)"\s?/>'
-        token.value = token.lexer.lexmatch.group(2)
+        token.value = token.lexer.lexmatch.group(4)
         return token
-
+    
+    def t_UNDERLINED(self, token):
+        r'[{]{2}u\|(.*?)[}]{2}'
+        token.value = token.lexer.lexmatch.group(6)
+        return token
+    
+    def t_BOLDED(self, token):
+        r"""'{3}(.*?)'{3}"""
+        token.value = token.lexer.lexmatch.group(6)
+        return token
+    
+    def t_ITALICIZED(self, token):
+        r"""'{2}(.*?)'{2}"""
+        token.value = token.lexer.lexmatch.group(6)
+        return token
+    
+    # Ignores
+    def t_EXTRANEOUS_HTML(self, token):
+        r'</?(?:div|p)(?:\s(?:.*?))?>'
+        pass
+    
     # Error handling
     def t_error(self, token):
         print ("Illegal character {}".format(token.value[0]))
         token.lexer.skip(1)
+        
+    def __init__(self):
+        self.logger = logging.getLogger("W2L")
     
     def analyze(self, data):
         self.lexer.input(data)
@@ -65,4 +131,4 @@ class Tokenizer(object):
             print(token)
         
     def build(self,**kwargs):
-        self.lexer = lex.lex(module=self, **kwargs)
+        self.lexer = lex.lex(module=self, reflags=re.DOTALL, **kwargs)
