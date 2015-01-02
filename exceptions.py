d9048d5e651b7c86df6d07d1543e170ebe5f61c9
Molly White <molly.white5@gmail.com>
2015-01-01 19:58:08 -0500

    Update copyright dates

--- exceptions.py
+++ exceptions.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2013 Molly White
+# Copyright (c) 2013–2015Molly White
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
